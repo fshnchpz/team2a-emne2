3eeff56e6d0bf0a0f190538b08d0bb0c16e5519b
Inetiger <ivensen18@gmail.com>
2023-10-18 10:13:11 +0200

    init

--- header/view.js
+++ header/view.js
@@ -1,31 +1,32 @@
 function getHTML_header() {
     let HTML = ``;
-    let LoginText = ``; 
-    let CurrentUserText = ``; 
-    
+    let LoginText = ``;
+    let CurrentUserText = ``;
+
     // Dette skjekker om brukeren er logget inn og vil derfor endre om det står logg inn eller logg ut.
     if (model.app.currentUser === '') {
         LoginText = 'Logg inn';
-    } else {
-        LoginText = 'Logg ut';
-    }
-
-    // Dette skjekker om brukeren er logget inn og vil derfor endre bruker som står på main linjen bruker/navn.
-    if (model.app.currentUser !== '') {
-        CurrentUserText = model.app.currentUser + '';
-    } else {
         CurrentUserText = `
         <div class="user_content">
             <div class="user_text">Bruker</div>
             <div class="acc_ico_user"/><div>
         </div>
         `;
+    } else {
+        LoginText = 'Logg ut';
+        CurrentUserText = model.app.currentUser + '';
     }
 
     HTML += /*HTML*/`
         
         <!-- Logg inn knappen -->
-        <div id="topMainLogInBtn" class="topMainLogInBtn" onclick=""><div class="LoginBTN_position"><img src="../images/TUR_Project_Account_Circle_Icon_2.png" class="acc_ico"></img><div id="login_txt" class="login_txt">${LoginText}</div></div></div>
+        <div id="topMainLogInBtn" class="topMainLogInBtn" onclick="">
+            <div class="LoginBTN_position">
+                <img src="../images/TUR_Project_Account_Circle_Icon_2.png" class="acc_ico">
+                <div id="login_txt" class="login_txt">${LoginText}
+                </div>
+            </div>
+        </div>
         <!-- Bakgrunnsbildet div -->
         <div class="topMainBackgroundImage">
             <!-- Bakgrunnsbildet -->
@@ -46,7 +47,6 @@ function getHTML_header() {
             <!-- Bruker knappen + account icon -->
             <div id="topMainPageAccountBtn" class="topMainPageAccountBtn">${CurrentUserText}</div>
         </div>
-    `; 
-
-    return HTML;
+    `;
+    document.getElementById('header').innerHTML = HTML;
 }
