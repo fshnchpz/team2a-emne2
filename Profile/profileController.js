4914180b27b6afc39c5ab942221cec6872168c3b
Inetiger <ivensen18@gmail.com>
2023-10-18 14:43:21 +0200

    init

--- Profile/profileController.js
+++ Profile/profileController.js
@@ -1,9 +1,29 @@
 "use strict";
 
-let html = '';
+function selection(list) {
+    let html = '';
 
-function showBox() {
-    if (document.getElementById('dropdown').value === 'choose') {
-        html = '';
+    if (list === 'favorite') {
+        html = /*html*/ `
+        <div class="content">
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+        </div>
+        `
+    } else if (list === 'trips') {
+        html = /*html*/ `
+        <div class="addBtn">button</div>
+        <div class="content">
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+            <div class="tur"></div>
+        </div>
+        `
     }
+    return html;
 }
