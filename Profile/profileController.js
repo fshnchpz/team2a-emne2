297de733c5c9e8af3a5cea3d50299a167645ff82
Inetiger <ivensen18@gmail.com>
2023-10-31 13:42:46 +0100

    init

--- Profile/profileController.js
+++ Profile/profileController.js
@@ -4,55 +4,43 @@ function selection(list) {
     let html = '';
 
     if (list === 'favorite') {
-        html = /*html*/ `
+        html = /*html*/`
         <div class="content">
-            ${writeFavorites()}
-        </div>
-        `
+            ${addMyFavoritesList()}
+        </div >
+        `;
     } else if (list === 'trips') {
-        html = /*html*/ `
-        <div class="addBtn" onclick="openCard()">Legg til ny tur</div>
+        html = /*html*/`
+        <div class="addBtn" >Legg til ny tur</div>
         <div class="content">
-            ${writeTrips()}
+            ${addMyTripsList()}
         </div>
-        `
+        `;
     }
     return html;
 }
 
-function openCard() {
-    let id_nb = model.data.trips.length
-    model.data.trips.push({
-        id: id_nb,
-        name: '',
-        about: '',
-        location: '',
-        distance: 0,        //number
-        time: 0,            //number
-        image: [],          //src/link
-        season: '',
-        // accessability: '',
-        map: '',            //src/link
-        difficulty: 0,      //number
-        parking: false,         //boolean
-        walking: false,        //boolean
-        wheelchair: false,      //boolean
-        bike: false,           //boolean
-        google_link: '',     //src/link
-        creator: model.app.currentUser,
-        approved: false,
-    })
-    console.log(id_nb)
-    turView_openCard(id_nb);
-    model.input.addEdit = !model.input.addEdit;
-    getHTML_turViewMode();
+function addMyFavoritesList() {
+    for (let user of model.data.users) {
+        if (user[model.app.currentUser]) {
+            console.log('nei')
+        }
+    }
+    return `
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    `;
 }
 
-function writeFavorites() {
-    for (let ary of model.data.users) {
-        console.log(model.app.currentUser)
-       if (model.app.currentUser === ary) {
-        console.log('hei')
-       }
-    }
+function addMyTripsList() {
+    return `
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    <div class="tur"></div>
+    `;
 }
